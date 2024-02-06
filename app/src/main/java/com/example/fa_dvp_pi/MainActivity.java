9e86b6716bb2d684afb434173da51aad7e809e38
prodbyhidden <denis.zak2003@gmail.com>
2024-02-06 21:20:33 +0300

    v = test_v_0.6.2

--- app/src/main/java/com/example/fa_dvp_pi/MainActivity.java
+++ app/src/main/java/com/example/fa_dvp_pi/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fa_dvp_pi;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.util.Log;
@@ -47,6 +48,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_first);
+
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        boolean isFirstRun = prefs.getBoolean("isFirstRun", true);
+
+
+        if (isFirstRun) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("isFirstRun", false);
+            editor.apply();
+        }
+        else {
+            Intent intent = new Intent(this, TimeTableActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
 
         mySpinner1 = (Spinner) findViewById(R.id.spinner1);
 
